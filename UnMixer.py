cc3d9f970c5a966631b2f0dfedb591bf6a287f24
Karl Lehenbauer <karl@lehenbauer.com>
2023-05-20 11:28:35 -0500

    Pretty much fix window resizing to be correct

--- UnMixer.py
+++ UnMixer.py
@@ -43,29 +43,45 @@ class MyGUI:
     def setup_gui(self):
         self.root.title("Unmixer - Stem and Backing Track Extractor")
 
-        next_row = 0  # Variable to track the next row position
+        self.frame1 = tk.Frame(self.root)
+        self.frame1.grid(row=0, column=0, sticky="nsew")
 
-        tk.Label(self.root, text="Stems", font=("Helvetica", 14, "bold")).grid(
+        self.frame2 = tk.Frame(self.root)
+        self.frame2.grid(row=1, column=0, sticky="nsew")
+
+        # GUI elements for frame1
+        # Stems, Backing Tracks, Labels, Filter, and Splitter
+
+        next_row = 0  # Variable to track the next row position in frame 1
+
+        tk.Label(self.frame1, text="Stems", font=("Helvetica", 14, "bold")).grid(
             row=next_row, column=0, sticky="w"
         )
-        tk.Label(self.root, text="Backing Tracks", font=("Helvetica", 14, "bold")).grid(
-            row=next_row, column=1, sticky="w"
-        )
+        tk.Label(
+            self.frame1, text="Backing Tracks", font=("Helvetica", 14, "bold")
+        ).grid(row=next_row, column=1, sticky="w")
         next_row += 1  # Increment the row position
 
         for i, element in enumerate(self.elements):
             self.stem_vars[element] = tk.BooleanVar()
             tk.Checkbutton(
-                self.root, text=element, variable=self.stem_vars[element]
+                self.frame1, text=element, variable=self.stem_vars[element]
             ).grid(row=next_row, column=0, sticky="w")
 
             self.backing_track_vars[element] = tk.BooleanVar()
             tk.Checkbutton(
-                self.root, text=element, variable=self.backing_track_vars[element]
+                self.frame1, text=element, variable=self.backing_track_vars[element]
             ).grid(row=next_row, column=1, sticky="w")
 
             # Status message label for each stem and backing track
-            status_label = tk.Label(self.root, text="", font=("Helvetica", 12), width=12)
+            status_label = tk.Label(
+                self.frame1,
+                text="",
+                font=("Helvetica", 12),
+                width=20,
+                relief="ridge",
+                padx=10,
+            )
             status_label.grid(row=next_row, column=2, sticky="ew")
             self.status_message[element] = status_label
 
@@ -75,84 +91,95 @@ class MyGUI:
         self.splitter = tk.StringVar()  # Initialize splitter variable
 
         # Filter and Splitter options
-        tk.Label(self.root, text="Filter", font=("Helvetica", 14, "bold")).grid(
+        tk.Label(self.frame1, text="Filter", font=("Helvetica", 14, "bold")).grid(
             row=next_row, column=0, sticky="w"
         )
-        tk.Label(self.root, text="Splitter", font=("Helvetica", 14, "bold")).grid(
+        tk.Label(self.frame1, text="Splitter", font=("Helvetica", 14, "bold")).grid(
             row=next_row, column=1, sticky="w"
         )
         next_row += 1
 
-        tk.Radiobutton(self.root, text="Mild", variable=self.filter, value=0).grid(
+        tk.Radiobutton(self.frame1, text="Mild", variable=self.filter, value=0).grid(
             row=next_row, column=0, sticky="w"
         )
         tk.Radiobutton(
-            self.root, text="Phoenix", variable=self.splitter, value="phoenix"
+            self.frame1, text="Phoenix", variable=self.splitter, value="phoenix"
         ).grid(row=next_row, column=1, sticky="w")
         next_row += 1
 
-        tk.Radiobutton(self.root, text="Normal", variable=self.filter, value=1).grid(
+        tk.Radiobutton(self.frame1, text="Normal", variable=self.filter, value=1).grid(
             row=next_row, column=0, sticky="w"
         )
         tk.Radiobutton(
-            self.root, text="Cassiopeia", variable=self.splitter, value="cassiopeia"
+            self.frame1, text="Cassiopeia", variable=self.splitter, value="cassiopeia"
         ).grid(row=next_row, column=1, sticky="w")
         next_row += 1
 
         tk.Radiobutton(
-            self.root, text="Aggressive", variable=self.filter, value=2
+            self.frame1, text="Aggressive", variable=self.filter, value=2
         ).grid(row=next_row, column=0, sticky="w")
         next_row += 1
 
+        # GUI elements for frame2
+        # API Key, Save to, Run
+
+        next_row = 0
+
         # API Key
-        tk.Label(self.root, text="API Key", font=("Helvetica", 14, "bold")).grid(
+        tk.Label(self.frame2, text="API Key", font=("Helvetica", 14, "bold")).grid(
             row=next_row, column=0, sticky="w"
         )
-        tk.Entry(self.root, textvariable=self.api_key, width=16).grid(
+        tk.Entry(self.frame2, textvariable=self.api_key, width=16).grid(
             row=next_row, column=1, sticky="we"
         )
-        tk.Button(self.root, text="Save", command=self.save_api_key).grid(
+        tk.Button(self.frame2, text="Save", command=self.save_api_key).grid(
             row=next_row, column=2
         )
         next_row += 1
 
         # Output dir
-        tk.Label(self.root, text="Save to", font=("Helvetica", 14, "bold")).grid(
+        tk.Label(self.frame2, text="Save to", font=("Helvetica", 14, "bold")).grid(
             row=next_row, column=0, sticky="w"
         )
-        tk.Entry(self.root, textvariable=self.tk_output_dir, width=16).grid(
+        tk.Entry(self.frame2, textvariable=self.tk_output_dir, width=16).grid(
             row=next_row, column=1, sticky="we"
         )
-        tk.Button(self.root, text="Change", command=self.set_output_dir).grid(
+        tk.Button(self.frame2, text="Change", command=self.set_output_dir).grid(
             row=next_row, column=2
         )
         next_row += 1
 
-        # Set default values
-        self.api_key.set(self.fetch_api_key())
-        self.filter.set(1)  # Default filter value
-        self.splitter.set("phoenix")  # Default splitter value
-        self.stem_vars["vocals"].set(True)
-        self.backing_track_vars["vocals"].set(True)
-        self.tk_output_dir.set(self.output_dir)
-
         tk.Button(self.root, text="Run", command=self.run_program).grid(
             row=next_row, column=0, columnspan=2
         )
+        next_row += 1
+
+        self.filter.set(1)
+        self.splitter.set("phoenix")
+        self.stem_vars["vocals"].set(True)
+        self.backing_track_vars["vocals"].set(True)
+
+        # Grid column configurations for frame1
+        self.frame1.grid_columnconfigure(0, weight=0)  # Column 0 not resizable
+        self.frame1.grid_columnconfigure(1, weight=0)  # Column 1 not resizable
+        self.frame1.grid_columnconfigure(2, weight=1)  # Column 2 resizable
+
+        # Grid column configurations for frame2
+        self.frame2.grid_columnconfigure(0, weight=0)  # Column 0 not resizable
+        self.frame2.grid_columnconfigure(1, weight=1)  # Column 1 resizable
+        self.frame2.grid_columnconfigure(2, weight=0)  # Column 2 not resizable
 
         # Configure grid to resize properly
-        self.root.grid_rowconfigure(
-            next_row + 1, weight=1
-        )  # Add row configuration for resizing
-        #self.root.grid_columnconfigure(
-        #    0, weight=1
-        #)  # Allow column 0 to resize horizontally
-        self.root.grid_columnconfigure(
-            1, weight=1
-        )  # Allow column 1 to resize horizontally
-        self.root.grid_columnconfigure(
-            2, weight=1
-        )  # Allow column 2 to resize horizontally
+        self.root.grid_rowconfigure(0, weight=1)  # Allow frame1 to resize vertically
+        self.root.grid_rowconfigure(1, weight=1)  # Allow frame2 to resize vertically
+        self.root.grid_columnconfigure(0, weight=1)  # Allow root to resize horizontally
+
+        self.root.update()  # Force update for the window to calculate its size
+
+        # set the minimum window size to the original size
+        width_height = self.root.geometry().split("+")[0]
+        width, height = map(int, width_height.split("x"))
+        self.root.minsize(width, height)
 
     def save_api_key(self):
         key = self.api_key.get().strip()
