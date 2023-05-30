bd545f866f72d4cb43f214a9aeedae76c8aa3d5c
Karl Lehenbauer <karl@lehenbauer.com>
2023-05-30 12:45:54 -0500

    close program on main window deletion

--- UnMixer.py
+++ UnMixer.py
@@ -1,12 +1,19 @@
 import os
+import sys
 import tkinter as tk
 
 import unmix
 
 create_console = True
 
+def on_close():
+    sys.exit(0)
+
 root = tk.Tk()
+root.protocol("WM_DELETE_WINDOW", on_close)
+
 root.my_gui = unmix.UnmixGUI(root)
+
 if create_console:
     unmix.create_console(tk, root.my_gui)
 print("yo baby yo baby yo")
